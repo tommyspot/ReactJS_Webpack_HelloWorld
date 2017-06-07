d330d288b5dcf6a8560954b6d1cb4e8e60e74e81
Thao Huynh <thaohuynh@kms.com.vn>
2017-06-07 16:42:48 +0700

    Init project

---
+++ main.js
@@ -0,0 +1,4 @@
+//https://www.twilio.com/blog/2015/08/setting-up-react-for-es6-with-webpack-and-babel-2.html
+
+import Hello from './jsx/hello.jsx';
+import World from './jsx/world.jsx';
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
